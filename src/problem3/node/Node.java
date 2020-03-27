d48932a07183aa5381ae296121b18a166e0cae64
shailja tripathi <shailja.tripathi_cs18@gla.ac.in>
2020-03-27 23:15:07 +0530

    getter setter methods

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -10,9 +10,32 @@ public class Node {
     private int data;
     private Node next;
     private int priority;
-    public Node(int data, int priority) {
+    public Node(int data , int priority) {
         this.data = data;
         this.next = null;
+        this.priority = priority;
+    }
+
+    public int getData() {
+        return data;
+    }
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
         this.priority = priority;
     }
 }
