c5b4d49d853dbb5271b5a093af2300e830c275c1
shailja tripathi <shailja.tripathi_cs18@gla.ac.in>
2020-03-27 23:25:16 +0530

    problem 3 completed

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,18 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+
+import problem3.myqueue.MyPriorityQueue;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+    public static void main(String[] args) {
+        MyPriorityQueue m = new MyPriorityQueue();
+        m.insert(30, 3);
+        m.insert(40, 4);
+        m.insert(10, 1);
+        m.insert(20, 2);
+        m.display();
+    }
 }
