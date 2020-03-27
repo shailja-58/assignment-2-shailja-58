890239b9aa4dcea90c197ae94e06327058415c6b
shailja tripathi <shailja.tripathi_cs18@gla.ac.in>
2020-03-27 23:57:46 +0530

    getter setter methods

--- src/problem5/node/Node.java
+++ src/problem5/node/Node.java
@@ -10,11 +10,28 @@ import problem5.student.Student;
 
 // to define node properties
 public class Node {
-    private Student  s;
-    private  Node next;
+    private Student s;
+    private Node next;
+
     public Node(Student s) {
         this.s = s;
         next = null;
     }
 
+    public Student getS() {
+        return s;
+    }
+    public void setS(Student s) {
+        this.s = s;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+}
 }
