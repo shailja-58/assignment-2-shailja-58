4be8b456cb3fadc8cf2113ce388fb13354cda700
shailja tripathi <shailja.tripathi_cs18@gla.ac.in>
2020-03-27 22:19:34 +0530

    bst class completed

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -6,77 +6,121 @@
  */
 package problem1.mybst;
 
-import javax.swing.tree.TreeNode;
+import problem1.node.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
 
 // to implement BinarySearchTree
-public class MyBinarySearchTree
-{
+public class MyBinarySearchTree {
     static Queue<TreeNode> q;
+    //constructor
+
+    //method to insert a element in tree
     public TreeNode insert(TreeNode root, int x) {
         if (root == null)
             return new TreeNode(x);
-        else if (x > root.getData())
+        else if (x > root.getData()) // x is greater. Should be inserted to right
             root.setRight(insert(root.getRight(), x));
-        else
+        else // x is smaller should be inserted to left
             root.setLeft(insert(root.getLeft(), x));
         return root;
     }
+
+    // A utility function to do inorder traversal of BST
     public void inorderRec(TreeNode root) {
         if (root.getLeft() != null) {
             inorderRec(root.getLeft());
             System.out.println(root.getData());
-            inorderRec(root.getRight()); }
-
+            inorderRec(root.getRight());
+        }
+    }
 
     public void countNodeWithOutLeftChild(TreeNode root) {
+        // If tree is empty
         if (root == null)
             return;
+
+        // Do level order traversal starting from root
         Queue<TreeNode> queue = new LinkedList<TreeNode>();
         queue.add(root);
 
-        int count = 0;
+        int count = 0; // Initialize count of half nodes
         while (queue != null) {
-             TreeNode temp = queue.poll();
-             if (temp == null) {
-                 break;
-             }
-             if (temp.getLeft() == null)
+            TreeNode temp = queue.poll();
+            if (temp == null) {
+                break;
+            }
+
+            if (temp.getLeft() == null)
                 count++;
-             if (temp.getLeft() != null)
+
+            // Enqueue left child
+            if (temp.getLeft() != null)
                 queue.add(temp.getLeft());
-             if (temp.getRight() != null)
+
+            // Enqueue right child
+            if (temp.getRight() != null)
                 queue.add(temp.getRight());
         }
-            System.out.println("count of Nodes without left child: " + count);
-        }
+        System.out.println("count of Nodes without left child: " + count);
+    }
+
+    // Utility function to print the left view of
+// the binary tree
     public void printLeftTree(TreeNode root) {
         if (root == null)
             return;
+
+        // add root
         q.add(root);
+
+        // Delimiter
         q.add(null);
+
         while (q.size() > 0) {
             TreeNode temp = q.peek();
 
             if (temp != null) {
+
+                // Prints first node
+                // of each level
                 System.out.print(temp.getData() + " ");
+
+                // add children of all nodes at
+                // current level
                 while (q.peek() != null) {
+
+                    // If left child is present
+                    // add into queue
                     if (temp.getLeft() != null)
                         q.add(temp.getLeft());
+
+                    // If right child is present
+                    // add into queue
                     if (temp.getRight() != null)
                         q.add(temp.getRight());
+
+                    // remove the current node
                     q.remove();
 
                     temp = q.peek();
                 }
                 q.add(null);
             }
+
+            // remove the delimiter of
+            // the previous level
             q.remove();
         }
     }
-        public void leftView(TreeNode root)
-        {
-            q = new LinkedList<TreeNode>();
-            printLeftTree(root);
-        }
+
+    public void leftView(TreeNode root) {
+        // Queue to store all
+        // the nodes of the tree
+        q = new LinkedList<TreeNode>();
+
+        printLeftTree(root);
     }
+
 }
--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -1,43 +1,36 @@
 package problem1.node;
 
-class TreeNode
-{
+class TreeNode{
     private int data;
-   private TreeNode left;
-   private TreeNode right;
-   public TreeNode(int data)
-   {
-       this.data = data;
-       left = null;
-       right = null;
-   }
-    public int getData()
-    {
-       return data;
+    private TreeNode left;
+    private TreeNode right;
+    public TreeNode(int data) {
+        this.data = data;
+        left = null;
+        right = null;
     }
 
-    public void setData(int data)
-    {
-       this.data = data;
+    public int getData() {
+        return data;
     }
 
-    public TreeNode getLeft()
-    {
-       return left;
+    public void setData(int data) {
+        this.data = data;
     }
 
-    public void setLeft(TreeNode left)
-    {
-       this.left = left;
+    public TreeNode getLeft() {
+        return left;
     }
 
-    public TreeNode getRight()
-    {
-       return right;
+    public void setLeft(TreeNode left) {
+        this.left = left;
     }
 
-    public void setRight(TreeNode right)
-    {
-       this.right = right;
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
     }
 }
