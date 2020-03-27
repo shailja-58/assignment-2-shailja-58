0a4ce50ea0b6eed1cba61937738a865923bf9546
shailja tripathi <shailja.tripathi_cs18@gla.ac.in>
2020-03-27 22:51:43 +0530

    main class of problem 2 is completed

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -5,8 +5,29 @@
  *  Time: 7:15 PM
  */
 package problem2.main;
+
+import problem1.mybst.MyBinarySearchTree;
+import problem1.node.TreeNode;
+
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+    public static void main(String [] args) {
+    TraversalOperations t = new TraversalOperations();
+    MyBinarySearchTree obj = new MyBinarySearchTree();
+    TreeNode root = new TreeNode(7);
+    obj.insert(root , 5);
+    obj.insert(root , 12);
+    obj.insert(root , 34);
+    obj.insert(root , 7);
+    obj.insert(root , 14);
+    obj.insert(root , 35);
+    obj.insert(root , 65);
+    t.printPreorder(root);
+    System.out.println();
+    t.printPostorder(root)
 
+
+
+    }
 }
