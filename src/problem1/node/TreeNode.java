4f142b17f84cc3c174edba6729727f806f45539d
shailja tripathi <shailja.tripathi_cs18@gla.ac.in>
2020-03-27 20:40:45 +0530

    getter and setter methods to access and initialise private variables

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -1,13 +1,43 @@
 package problem1.node;
 
-class TreeNode {
+class TreeNode
+{
     private int data;
    private TreeNode left;
    private TreeNode right;
-   private TreeNode root;
-   public TreeNode(int data) {
+   public TreeNode(int data)
+   {
        this.data = data;
        left = null;
        right = null;
+   }
+    public int getData()
+    {
+       return data;
+    }
+
+    public void setData(int data)
+    {
+       this.data = data;
+    }
+
+    public TreeNode getLeft()
+    {
+       return left;
+    }
+
+    public void setLeft(TreeNode left)
+    {
+       this.left = left;
+    }
+
+    public TreeNode getRight()
+    {
+       return right;
+    }
+
+    public void setRight(TreeNode right)
+    {
+       this.right = right;
     }
 }
