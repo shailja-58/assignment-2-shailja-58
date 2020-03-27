3c2db2d58c177ba1e70c53b8d63fa9a20de8b8cf
shailja tripathi <shailja.tripathi_cs18@gla.ac.in>
2020-03-27 21:08:31 +0530

    Initializing count of half nodes

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -34,6 +34,12 @@ public class MyBinarySearchTree
         Queue<TreeNode> queue = new LinkedList<TreeNode>();
         queue.add(root);
 
+        int count = 0;
+        while (queue != null) {
+             TreeNode temp = queue.poll();
+             if (temp == null) {
+                 break;
+             }
 
 
         }
