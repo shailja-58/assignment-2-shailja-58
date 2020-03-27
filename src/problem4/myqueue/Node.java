528217efdbb10129382e84d6ac457c9d4588b895
shailja tripathi <shailja.tripathi_cs18@gla.ac.in>
2020-03-27 23:30:07 +0530

    getter setter methods

--- src/problem4/myqueue/Node.java
+++ src/problem4/myqueue/Node.java
@@ -13,4 +13,19 @@ public class Node {
         node = treenode;
         next = null;
     }
+    public TreeNode getNode() {
+        return node;
+    }
+
+    public void setNode(TreeNode node) {
+        this.node = node;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
