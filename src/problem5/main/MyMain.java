61e98db2edc57c12536650b23550fa86e32a5e2c
shailja tripathi <shailja.tripathi_cs18@gla.ac.in>
2020-03-28 00:08:10 +0530

    main class completed

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -5,6 +5,28 @@
  *  Time: 7:06 PM
  */
 package problem5.main;
+
+import problem5.circularqueue.MyCircularQueue;
+import problem5.node.Node;
+import problem5.student.Student;
+
+import java.util.Scanner;
+
 //executable class
 public class MyMain {
+    public static void main(String[] args) {
+        MyCircularQueue m = new MyCircularQueue();
+        Node node;
+
+        for (int i = 0; i < 5; i++) {
+            node = new Node(new Student());
+            m.enqueue(node);
+        }
+        m.printQueue();
+
+        m.remove(new Scanner(System.in).next());
+        m.printQueue();
+
+        m.process(new Scanner(System.in).next());
+    }
 }
