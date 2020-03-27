4217b276d712d762360fcb18e4c5cbfc651b4aa1
shailja tripathi <shailja.tripathi_cs18@gla.ac.in>
2020-03-28 00:01:45 +0530

    initializing the private fields

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -5,6 +5,13 @@
  *  Time: 7:06 PM
  */
 package problem5.student;
+
+import java.util.Scanner;
+
 // to store student information and properties
 public class Student {
+    private String name;
+    private int rollno, backlog, apperingcount;
+    private Scanner sc;
+
 }
