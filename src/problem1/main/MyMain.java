a8fd70c1dd9def12daea31dac927ce62f5a3d5a5
shailja tripathi <shailja.tripathi_cs18@gla.ac.in>
2020-03-27 21:48:23 +0530

    main class completed

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -7,5 +7,26 @@
 package problem1.main;
 // executable class
 
-public class MyMain {
-}
+import problem1.mybst.MyBinarySearchTree;
+
+import javax.swing.tree.TreeNode;
+import java.util.Enumeration;
+
+public abstract class MyMain {
+    public static void main(String [] args) {
+        MyBinarySearchTree obj = new MyBinarySearchTree();
+        TreeNode root;
+        root = new TreeNode(8);
+        obj.insert(root , 6);
+        obj.insert(root , 3);
+        obj.insert(root , 5);
+        obj.insert(root , 7);
+        obj.insert(root , 9);
+        obj.insert(root , 2);
+        obj.insert(root , 1);
+        obj.insert(root , 11);
+        obj.leftView(root);
+
+        }
+    }
+
