18d4b10a6b73ae32e46d6a2e4413251ca9bbdc0c
shailja tripathi <shailja.tripathi_cs18@gla.ac.in>
2020-03-28 00:13:07 +0530

    process

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -73,4 +73,18 @@ public class MyCircularQueue {
             tmp.setNext(tmp.getNext().getNext());
         }
     }
+    public void process(String name) {
+        tmp = front;
+        if (tmp.getS().getName().equals(name)) {
+            System.out.println(tmp.getS().toString());
+            System.out.println(tmp.getS().getBacklog() - tmp.getS().getApperingcount());
+        }
+        while (!tmp.getS().getName().equals(name)) {
+            tmp = tmp.getNext();
+            if (tmp == front)
+                return;
+        }
+        System.out.println(tmp.getS().toString());
+        System.out.println(tmp.getS().getBacklog() - tmp.getS().getApperingcount());
+    }
 }
