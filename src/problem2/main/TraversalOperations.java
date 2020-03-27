3db95d3ea6192c9f93ed031a779c4ccc1e51d46a
shailja tripathi <shailja.tripathi_cs18@gla.ac.in>
2020-03-27 22:56:33 +0530

    created the class to perform traversal operations

--- src/problem2/main/TraversalOperations.java
+++ src/problem2/main/TraversalOperations.java
@@ -1,4 +1,13 @@
 package problem2.main;
 
+import problem1.node.TreeNode;
+
 public class TraversalOperations {
+    void printPostorder(TreeNode node) {
+        if(node == null)
+            return;
+        printPostorder(node.getLeft());
+        printPostorder(node.getRight());
+        System.out.println(node.getData() + " ");
+    }
 }
