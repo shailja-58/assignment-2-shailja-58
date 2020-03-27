5cf503fe11d5984ca96110e941bba9c7dcf7817e
shailja tripathi <shailja.tripathi_cs18@gla.ac.in>
2020-03-27 23:32:01 +0530

    taking helper variables and initializing it

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -8,4 +8,14 @@ package problem4.myqueue;
 // to create queue to store pre - order successor
 
 public class MyQueue {
+    private Node front, end, tmp;
+    private int size;
+
+
+    public MyQueue() {
+        front = null;
+        end = null;
+        tmp = null;
+        size = 0;
+    }
 }
