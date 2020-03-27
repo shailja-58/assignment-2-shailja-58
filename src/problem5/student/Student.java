913db923ac4ab341f31709c94b2eeb568b4f2609
shailja tripathi <shailja.tripathi_cs18@gla.ac.in>
2020-03-28 00:02:15 +0530

    references

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -13,5 +13,17 @@ public class Student {
     private String name;
     private int rollno, backlog, apperingcount;
     private Scanner sc;
+    public Student() {
+        sc = new Scanner(System.in);
+        System.out.print("Name :");
+        this.name = sc.next();
+        System.out.print("Roll.no : ");
+        this.rollno = sc.nextInt();
+        System.out.print("Backlog_count : ");
+        this.backlog = sc.nextInt();
+        System.out.print("appering_count");
+        this.apperingcount = sc.nextInt();
 
+
+    }
 }
