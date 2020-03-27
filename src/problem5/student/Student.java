0cf8a52adec0a807ba7c28a26546a8dbbd820c14
shailja tripathi <shailja.tripathi_cs18@gla.ac.in>
2020-03-28 00:04:08 +0530

    getter setter methods and override string

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -23,7 +23,45 @@ public class Student {
         this.backlog = sc.nextInt();
         System.out.print("appering_count");
         this.apperingcount = sc.nextInt();
-
-
     }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRollno() {
+        return rollno;
+    }
+
+    public void setRollno(int rollno) {
+        this.rollno = rollno;
+    }
+
+    @Override
+    public String toString() {
+        return "name='" + name + '\'' +
+                ", rollno=" + rollno +
+                ", backlog=" + backlog +
+                ", apperingcount=" + apperingcount
+                ;
+    }
+
+    public int getBacklog() {
+        return backlog;
+    }
+
+    public void setBacklog(int backlog) {
+        this.backlog = backlog;
+    }
+
+    public int getApperingcount() {
+        return apperingcount;
+    }
+
+    public void setApperingcount(int apperingcount) {
+        this.apperingcount = apperingcount;
+    }
+
 }
